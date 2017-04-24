0b3894073f0ea8ce8468d52bc5b031e89dfc8006
Ivan Tkachenko <ivan.tkachenko@youshido.com>
2017-04-24 15:29:27 +0300

    Initial Commit

---
+++ Callee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Callee
+//
+//  Created by Ivan Tkachenko on 4/24/17.
+//  Copyright © 2017 Ivan Tkachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Callee/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Callee
+//
+//  Created by Ivan Tkachenko on 4/24/17.
+//  Copyright © 2017 Ivan Tkachenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
